78d28814116dfe823577ea844b3e42b643df892f
moskowitza <moskowitzaaron@mac.com>
2019-07-22 08:25:13 -0400

    card list test

--- src/components/CardList.test.js
+++ src/components/CardList.test.js
@@ -1,22 +1,32 @@
-import { shallow, mount, render } from 'enzyme';
+import {
+  shallow, mount, render, configure,
+} from 'enzyme';
 import React from 'react';
 
+import Adapter from 'enzyme-adapter-react-16';
 import CardList from './CardList';
+import RobotCard from './RobotCard';
 
 
-test('cardList snapshot', () => {
-  const mockRobots = [{
-    id: 1,
-    name: 'fred',
-    email: 'fred@email.com',
-  },
-  {
-    id: 2,
-    name: 'fred',
-    email: 'fred@email.com',
-  },
-  ];
+configure({ adapter: new Adapter() });
+
+const mockRobots = [{
+  id: 1,
+  name: 'fred',
+  email: 'fred@email.com',
+},
+{
+  id: 2,
+  name: 'fred',
+  email: 'fred@email.com',
+},
+];
+it('cardList snapshot', () => {
   // eslint-disable-next-line no-undef
-  const cardList = shallow(<CardList robots={mockRobots} />);
+  const cardList = render(<CardList robots={mockRobots} />);
   expect(cardList).toMatchSnapshot();
+});
+it('It Renders 2 robotCards', () => {
+  const wrapper = shallow(<CardList robots={mockRobots} />);
+  expect(wrapper.find(RobotCard).length).toBe(2);
 });
